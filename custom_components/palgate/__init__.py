bd373b304472ce3d2fc9cd6e0edb975836c7522a
doron1 <4199618+doron1@users.noreply.github.com>
2025-03-09 15:10:23 +0200

    Avoid error message when unloading an entry (remove obsolete pop)

--- custom_components/palgate/__init__.py
+++ custom_components/palgate/__init__.py
@@ -23,9 +23,6 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-    if unload_ok:
-        hass.data[PALGATE_DOMAIN].pop(entry.entry_id)
-
     return unload_ok
 
 
